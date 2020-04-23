bef975e401650f78d21eebbdf1e7d5572f9b3212
Jacob <36286032+Hircinus@users.noreply.github.com>
2020-04-23 15:20:16 -0400

    Add files via upload

---
+++ docs/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Chatbot</title>
+</head>
+<body>
+  <h1>Chat with Proto</h1>
+  <form method="post">
+    <input id="message" name="message" placeholder="Your message here">
+    <button type="submit" name="submit" onclick="message()">Submit</button>
+  </form>
+  <div class="chat">
+
+  </div>
+  <script>
+
+  </script>
+</body>
+</html>
